0c9eaaafa387b5fb8e81d27d0a8e75bbc0809971
EJ <enickols@nickolstechnologies.com>
2020-05-17 20:21:03 -0500

    Adding first console app to GIT

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+/*
+**An interface is like a contract. In the human world, the contract between the two or more 
+**humans binds them to act as per the contract. In the same way, the interface includes the 
+**declaration of one or more functionalities. Entities that implement the interface must define 
+**functionalities declared in the interface. In C#, a class or a struct can implement one or more interfaces.
+
+**In C#, an interface can be defined using the interface keyword. Interfaces can contain methods, 
+**properties, indexers, and events as members.
+
+**Consider the following IPen interface that declares some basic functionality for a pen.
+*/
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"This is some additional text added by Earlie Nickols, III");
+        }
+    }
+}
